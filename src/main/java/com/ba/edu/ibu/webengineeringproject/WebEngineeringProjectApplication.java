5c5f4fce518c7f42c64097dfa2fc3e84fb7dd8f0
amulalic <amulalic@apple.com>
2023-10-08 21:41:09 +0200

    Add initial structure

---
+++ src/main/java/com/ba/edu/ibu/webengineeringproject/WebEngineeringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ba.edu.ibu.webengineeringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEngineeringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEngineeringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ba/edu/ibu/webengineeringproject/WebEngineeringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ba.edu.ibu.webengineeringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEngineeringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
