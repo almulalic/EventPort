4280e44c4b6914699301b6a3beaf1df7bc708c68
amulalic <amulalic@apple.com>
2023-10-22 22:28:01 +0200

    Add ticket rest models

--- src/main/java/ba/edu/ibu/eventport/rest/models/dto/TicketDTO.java
+++ src/main/java/ba/edu/ibu/eventport/rest/models/dto/TicketDTO.java
@@ -1,2 +1,69 @@
-package ba.edu.ibu.eventport.rest.models.dto;public class TicketDTO {
+package ba.edu.ibu.eventport.rest.models.dto;
+
+import ba.edu.ibu.eventport.core.model.Ticket;
+
+public class TicketDTO {
+  private String id;
+  private int eventId;
+  private int userId;
+  private String type;
+  private double price;
+  private boolean isRegistered;
+
+  public TicketDTO(Ticket ticket) {
+    this.id = ticket.getId();
+    this.eventId = ticket.getEventId();
+    this.userId = ticket.getUserId();
+    this.type = ticket.getType();
+    this.price = ticket.getPrice();
+    this.isRegistered = ticket.isRegistered();
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public int getEventId() {
+    return eventId;
+  }
+
+  public void setEventId(int eventId) {
+    this.eventId = eventId;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public boolean isRegistered() {
+    return isRegistered;
+  }
+
+  public void setRegistered(boolean registered) {
+    isRegistered = registered;
+  }
 }
--- src/main/java/ba/edu/ibu/eventport/rest/models/dto/TicketRequestDTO.java
+++ src/main/java/ba/edu/ibu/eventport/rest/models/dto/TicketRequestDTO.java
@@ -1,2 +1,70 @@
-package ba.edu.ibu.eventport.rest.models.dto;public class TicketRequestDTO {
+package ba.edu.ibu.eventport.rest.models.dto;
+
+import ba.edu.ibu.eventport.core.model.Event;
+import ba.edu.ibu.eventport.core.model.Ticket;
+
+public class TicketRequestDTO {
+  private int eventId;
+  private int userId;
+  private String type;
+  private double price;
+  private boolean isRegistered;
+
+  public TicketRequestDTO(int eventId, int userId, String type, double price, boolean isRegistered) {
+    this.eventId = eventId;
+    this.userId = userId;
+    this.type = type;
+    this.price = price;
+    this.isRegistered = isRegistered;
+  }
+
+  public Ticket toEntity() {
+    Ticket ticket = new Ticket();
+    ticket.setEventId(eventId);
+    ticket.setUserId(userId);
+    ticket.setType(type);
+    ticket.setPrice(price);
+    ticket.setRegistered(isRegistered);
+    return ticket;
+  }
+
+  public int getEventId() {
+    return eventId;
+  }
+
+  public void setEventId(int eventId) {
+    this.eventId = eventId;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public boolean isRegistered() {
+    return isRegistered;
+  }
+
+  public void setRegistered(boolean registered) {
+    isRegistered = registered;
+  }
 }
