7a1e17a0b4bb126bb8b33f145f5149a3a61df524
amulalic <amulalic@apple.com>
2023-10-22 12:00:21 +0200

    Rename application and model file to reflect project name

---
+++ src/main/java/ba/edu/ibu/eventport/EventPortApplication.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.eventport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPortApplication.class, args);
+    }
+
+}
