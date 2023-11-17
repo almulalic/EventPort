4ce0e8fd721129e6bc0133ad69faa3e654eeaf0e
amulalic <amulalic@apple.com>
2023-11-17 17:13:42 +0100

    Add spring security implementations

--- auth/src/main/java/ba/edu/ibu/eventport/auth/rest/filter/JWTAuthenticationFilter.java
+++ auth/src/main/java/ba/edu/ibu/eventport/auth/rest/filter/JWTAuthenticationFilter.java
@@ -1,2 +1,62 @@
-package ba.edu.ibu.eventport.auth.rest.filter;public class JWTAuthenticationFilter {
+package ba.edu.ibu.eventport.auth.rest.filter;
+
+import ba.edu.ibu.eventport.auth.core.service.JWTService;
+import ba.edu.ibu.eventport.auth.core.service.UserService;
+import com.mongodb.lang.NonNull;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+  @Autowired
+  private JWTService jwtService;
+
+  @Autowired
+  private UserService userService;
+
+  @Override
+  protected void doFilterInternal(
+    @NonNull HttpServletRequest request,
+    @NonNull HttpServletResponse response,
+    @NonNull FilterChain filterChain
+  )
+    throws ServletException, IOException, ServletException, IOException {
+    final String authHeader = request.getHeader("Authorization");
+    final String jwt;
+    final String userEmail;
+    if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
+      filterChain.doFilter(request, response);
+      return;
+    }
+    jwt = authHeader.substring(7);
+    userEmail = jwtService.extractUserName(jwt);
+    if (StringUtils.isNotEmpty(userEmail)
+        && SecurityContextHolder.getContext()
+             .getAuthentication() == null) {
+      UserDetails userDetails = userService.userDetailsService()
+                                  .loadUserByUsername(userEmail);
+      if (jwtService.isTokenValid(jwt, userDetails)) {
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+          userDetails, null, userDetails.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        context.setAuthentication(authToken);
+        SecurityContextHolder.setContext(context);
+      }
+    }
+    filterChain.doFilter(request, response);
+  }
 }
