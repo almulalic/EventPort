ebb9b0edd699184e63a1c8e6fb1c06eb95df24d2
amulalic <amulalic@apple.com>
2023-11-24 11:31:57 +0100

    Add eureka registry for handling microservices

---
+++ registry/src/test/java/ba/edu/ibu/eventport/api/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.edu.ibu.eventport.api.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
