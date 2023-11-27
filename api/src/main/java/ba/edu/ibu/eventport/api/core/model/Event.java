ac38b36ce842d4b471f3b53c62cc4468c36c6a0a
amulalic <amulalic@apple.com>
2023-11-27 01:40:12 +0100

    Refactor model to use lombock

--- api/src/main/java/ba/edu/ibu/eventport/api/core/model/Event.java
+++ api/src/main/java/ba/edu/ibu/eventport/api/core/model/Event.java
@@ -1,150 +1,60 @@
 package ba.edu.ibu.eventport.api.core.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import ba.edu.ibu.eventport.api.core.model.enums.EventStatus;
 
 import java.util.Date;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
   @Id
+  @Getter
+  @Setter
   private String id;
+
+  @Getter
+  @Setter
   private String name;
+
+  @Getter
+  @Setter
   private String description;
+
+  @Getter
+  @Setter
   private Date dateTime;
+
+  @Getter
+  @Setter
   private String location;
-  private String organizer;
+
+  @Getter
+  @Setter
   private List<Integer> participants;
+
+  @Getter
+  @Setter
   private String type;
+
+  @Getter
+  @Setter
   private EventStatus status;
+
+  @Getter
+  @Setter
   private int capacity;
+
+  @Getter
+  @Setter
   private Date registrationDeadline;
+
+  @Getter
+  @Setter
   private String bannerImageURL;
-
-  public Event() {
-  }
-
-  public Event(
-    String id,
-    String name,
-    String description,
-    Date dateTime,
-    String location,
-    String organizer,
-    List<Integer> participants,
-    String type,
-    EventStatus status,
-    int capacity,
-    Date registrationDeadline,
-    String bannerImageURL
-  ) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.dateTime = dateTime;
-    this.location = location;
-    this.organizer = organizer;
-    this.participants = participants;
-    this.type = type;
-    this.status = status;
-    this.capacity = capacity;
-    this.registrationDeadline = registrationDeadline;
-    this.bannerImageURL = bannerImageURL;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-  public Date getDateTime() {
-    return dateTime;
-  }
-
-  public void setDateTime(Date dateTime) {
-    this.dateTime = dateTime;
-  }
-
-  public String getLocation() {
-    return location;
-  }
-
-  public void setLocation(String location) {
-    this.location = location;
-  }
-
-  public String getOrganizer() {
-    return organizer;
-  }
-
-  public void setOrganizer(String organizer) {
-    this.organizer = organizer;
-  }
-
-  public List<Integer> getParticipants() {
-    return participants;
-  }
-
-  public void setParticipants(List<Integer> participants) {
-    this.participants = participants;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public EventStatus getStatus() {
-    return status;
-  }
-
-  public void setStatus(EventStatus status) {
-    this.status = status;
-  }
-
-  public int getCapacity() {
-    return capacity;
-  }
-
-  public void setCapacity(int capacity) {
-    this.capacity = capacity;
-  }
-
-  public Date getRegistrationDeadline() {
-    return registrationDeadline;
-  }
-
-  public void setRegistrationDeadline(Date registrationDeadline) {
-    this.registrationDeadline = registrationDeadline;
-  }
-
-  public String getBannerImageURL() {
-    return bannerImageURL;
-  }
-
-  public void setBannerImageURL(String bannerImageURL) {
-    this.bannerImageURL = bannerImageURL;
-  }
 }
--- api/src/main/java/ba/edu/ibu/eventport/api/core/model/Ticket.java
+++ api/src/main/java/ba/edu/ibu/eventport/api/core/model/Ticket.java
@@ -1,70 +1,34 @@
 package ba.edu.ibu.eventport.api.core.model;
 
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ticket {
+  @Getter
+  @Setter
   private String id;
+
+  @Getter
+  @Setter
   private String eventId;
+
+  @Getter
+  @Setter
   private String userId;
+
+  @Getter
+  @Setter
   private String type;
+
+  @Getter
+  @Setter
   private double price;
+
+  @Getter
+  @Setter
   private boolean isRegistered;
-
-  public Ticket() {
-  }
-
-  public Ticket(String id, String eventId, String userId, String type, double price, boolean isRegistered) {
-    this.id = id;
-    this.eventId = eventId;
-    this.userId = userId;
-    this.type = type;
-    this.price = price;
-    this.isRegistered = isRegistered;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getEventId() {
-    return eventId;
-  }
-
-  public void setEventId(String eventId) {
-    this.eventId = eventId;
-  }
-
-  public String getUserId() {
-    return userId;
-  }
-
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
-
-  public String getType() {
-    return type;
-  }
-
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  public double getPrice() {
-    return price;
-  }
-
-  public void setPrice(double price) {
-    this.price = price;
-  }
-
-  public boolean isRegistered() {
-    return isRegistered;
-  }
-
-  public void setRegistered(boolean registered) {
-    isRegistered = registered;
-  }
 }
