4335da0c49041f9e906fa7d729769d1db7c155be
amulalic <amulalic@apple.com>
2023-11-27 02:14:24 +0100

    Remove unecessary properties from EventRepository

--- api/src/main/java/ba/edu/ibu/eventport/api/core/repository/EventRepository.java
+++ api/src/main/java/ba/edu/ibu/eventport/api/core/repository/EventRepository.java
@@ -3,12 +3,9 @@ package ba.edu.ibu.eventport.api.core.repository;
 import java.util.List;
 
 import ba.edu.ibu.eventport.api.core.model.Event;
-import ba.edu.ibu.eventport.api.core.model.enums.EventStatus;
-import ba.edu.ibu.eventport.api.core.repository.generics.filtering.FilterableRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import ba.edu.ibu.eventport.api.core.repository.generics.filtering.FilterableRepository;
 
 public interface EventRepository extends MongoRepository<Event, String>,
                                          FilterableRepository<Event> {
@@ -20,6 +17,4 @@ public interface EventRepository extends MongoRepository<Event, String>,
     }
   )
   List<Event> findByCategory(String category);
-
-  Page<Event> findByTypeAndStatus(String type, EventStatus status, Pageable pageable);
 }
