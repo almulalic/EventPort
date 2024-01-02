824ad6330d03c774748452e55abc2a1d2ad1a4ac
almulalic <almir.mulalic.am@gmail.com>
2024-01-02 17:16:59 +0100

    Refactor main layout

--- frontend/src/containers/mainLayout/MainLayout.tsx
+++ frontend/src/containers/mainLayout/MainLayout.tsx
@@ -1,47 +1,35 @@
-import { Layout, Menu } from "antd";
+import { ConfigProvider, Layout, Menu } from "antd";
 import EventSlideshow from "../../components/EventSlideshow/EventSlideshow";
 import { EventSection } from "../../components/EventSection/EventSection";
+import { HowItWorks } from "../../components/HowItWorks/HowItWorks";
+import Navbar from "../Navbar/Navbar";
+
 import "./MainLayout.scss";
 
-const { Header, Content, Footer } = Layout;
-
-const items = [
-	{
-		key: 0,
-		label: <a>How it works</a>,
-	},
-	{
-		key: 1,
-		label: <a>Events</a>,
-	},
-	{
-		key: 2,
-		label: <a>Create Event</a>,
-	},
-	{
-		key: 3,
-		label: <a>Log In</a>,
-	},
-];
+const { Content, Footer } = Layout;
 
 const MainLayout: React.FC = () => {
 	return (
-		<Layout className="MainLayout">
-			<Header className="MainLayout-Header">
-				<div className="MainLayout-Header-Logo" />
-				<Menu
-					theme="dark"
-					mode="horizontal"
-					items={items}
-					style={{ flex: items.length, minWidth: 0, justifyContent: "flex-end" }}
-				/>
-			</Header>
-			<Content className="MainLayout-Content">
-				<EventSlideshow />
-				<EventSection />
-			</Content>
-			<Footer style={{ textAlign: "center" }}>Ant Design ©2023 Created by Ant UED</Footer>
-		</Layout>
+		<ConfigProvider
+			theme={{
+				components: {
+					Layout: {
+						headerHeight: 40,
+						headerPadding: "2rem 3.5rem",
+					},
+				},
+			}}
+		>
+			<Layout className="MainLayout">
+				<Navbar />
+				<Content className="MainLayout-Content">
+					<EventSlideshow />
+					<EventSection />
+					<HowItWorks />
+				</Content>
+				<Footer style={{ textAlign: "center" }}>Ant Design ©2023 Created by Ant UED</Footer>
+			</Layout>
+		</ConfigProvider>
 	);
 };
 
