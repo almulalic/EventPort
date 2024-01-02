5113ff3243d97d484e6c162760916f0158584718
almulalic <almir.mulalic.am@gmail.com>
2024-01-02 18:46:02 +0100

    Refactor event slideshow

--- frontend/src/components/EventSlideshow/EventSlideshow.tsx
+++ frontend/src/components/EventSlideshow/EventSlideshow.tsx
@@ -6,8 +6,8 @@ import "./EventSlideshow.scss";
 const eventPictureUrls = [
 	"https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
 	"https://teatrocolon.org.ar/sites/default/files/styles/d10/public/teatro-colon-argentina-image_1366_768.jpg?itok=0sbBdjrl",
-	"https://picsum.photos/800/400/?image=1015",
-	"https://picsum.photos/800/400/?image=1018",
+	"https://cinema.cornell.edu/sites/default/files/styles/pano/public/2023-12/pxl20231207001156309night.jpg?h=aba3a455&itok=vfGZGo7G",
+	"https://images.unsplash.com/photo-1578736641330-3155e606cd40?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D8",
 ];
 
 const EventSlideshow: React.FC = () => {
@@ -19,11 +19,7 @@ const EventSlideshow: React.FC = () => {
 				{eventPictureUrls.map((url, index) => (
 					<div key={index}>
 						<div id="overlay"></div>
-						<img
-							src={url}
-							alt={`Event ${index + 1}`}
-							style={{ position: "relative", width: "100%", height: "720px" }}
-						/>
+						<img src={url} className="EventSlideshow-Picture" alt={`Event ${index + 1}`} />
 					</div>
 				))}
 			</Carousel>
