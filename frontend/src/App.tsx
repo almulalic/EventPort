44399d5eb1557e76ef9674b2f768095e17b1009a
almulalic <almir.mulalic.am@gmail.com>
2024-01-04 23:25:48 +0100

    Fix global html/css issues, remove unused files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,35 @@
-import { ConfigProvider } from "antd";
+import { ConfigProvider, theme } from "antd";
 import MainLayout from "./containers/MainLayout/MainLayout";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing/Landing";
+import Login from "./pages/Login/Login";
 
 function App() {
 	return (
 		<ConfigProvider
 			theme={{
+				algorithm: theme.darkAlgorithm,
 				token: {
-					colorPrimary: "#161616",
-					colorPrimaryBgHover: "#313131",
-					colorPrimaryBorder: "#313131",
-					colorTextBase: "#ffffff",
-					colorBgContainer: "#161616",
-					colorBorder: "#212121",
-					colorBgElevated: "#212121",
+					colorBgMask: "rgba(110, 220, 142, 0.45)",
+					colorBgSpotlight: "rgba(211, 122, 213, 0.85)",
+
+					colorPrimary: "#f29727",
+					colorPrimaryActive: "#b86e00",
+					colorPrimaryBg: "#f29727",
+					colorPrimaryBgHover: "#f5e3c6",
+					colorPrimaryBorder: "#f9dec6",
+					colorPrimaryBorderHover: "#f2c17a",
+					colorPrimaryHover: "#e08200",
+					colorPrimaryText: "#cc7000",
+					colorPrimaryTextActive: "#b86e00",
+					colorPrimaryTextHover: "#e08200",
 				},
 			}}
 		>
-			<MainLayout />
+			<Routes>
+				<Route path="/" element={<Login />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
 		</ConfigProvider>
 	);
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import React from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import "./index.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Provider store={store}>
+					<App />
+				</Provider>
+			</BrowserRouter>
+		</QueryClientProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
