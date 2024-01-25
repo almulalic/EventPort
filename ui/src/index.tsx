23c1d3dc1b858f73f740d0ed74322f5b9733eec0
almulalic <almir.mulalic.am@gmail.com>
2024-01-26 00:16:32 +0100

    Dockerfile fix

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,11 +1,10 @@
-require("file-loader?name=[name].[ext]!./index.html");
+require("file-loader?name=[name].[ext]!./public/index.html");
 
 import App from "./App";
 import React from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 
@@ -24,8 +23,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 		</QueryClientProvider>
 	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -2,54 +2,52 @@ const path = require("path");
 const webpack = require("webpack");
 require("dotenv").config({ path: "./.env" });
 
-module.exports = (env) => {
-	return {
-		entry: "./src/index.tsx",
-		output: {
-			path: path.resolve(__dirname, "dist"),
-			filename: "index.bundle.js",
-		},
-		devServer: {
-			port: 3000,
-			allowedHosts: "all",
-		},
-		resolve: {
-			modules: [__dirname, "src", "node_modules"],
-			extensions: [".js", ".jsx", ".tsx", ".ts", ".*"],
-		},
-		plugins: [
-			new webpack.DefinePlugin({
-				"process.env": JSON.stringify(process.env),
-			}),
+module.exports = {
+	entry: "./src/index.tsx",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "index.bundle.js",
+	},
+	devServer: {
+		port: 3000,
+		allowedHosts: "all",
+	},
+	resolve: {
+		modules: [__dirname, "src", "node_modules", "public"],
+		extensions: [".js", ".jsx", ".tsx", ".ts", ".*"],
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			"process.env": JSON.stringify(process.env),
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|ts)x?$/,
+				exclude: /node_modules/,
+				use: ["babel-loader"],
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				exclude: /node_modules/,
+				use: ["file-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+				exclude: /node_modules/,
+				use: ["file-loader?name=[name].[ext]"],
+			},
 		],
-		module: {
-			rules: [
-				{
-					test: /\.(js|ts)x?$/,
-					exclude: /node_modules/,
-					use: ["babel-loader"],
-				},
-				{
-					test: /\.css$/,
-					exclude: /node_modules/,
-					use: ["style-loader", "css-loader"],
-				},
-				{
-					test: /\.scss$/,
-					exclude: /node_modules/,
-					use: ["style-loader", "css-loader", "sass-loader"],
-				},
-				{
-					test: /\.(png|svg|jpg|gif)$/,
-					exclude: /node_modules/,
-					use: ["file-loader"],
-				},
-				{
-					test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-					exclude: /node_modules/,
-					use: ["file-loader?name=[name].[ext]"],
-				},
-			],
-		},
-	};
+	},
 };
